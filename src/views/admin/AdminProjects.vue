3a476bec75ac6f3686fe977fb44029f3efebf712
nereid-n <nas.kuleshova@gmail.com>
2019-02-18 16:50:25 +0300

    add table

--- src/views/admin/AdminProjects.vue
+++ src/views/admin/AdminProjects.vue
@@ -1,10 +1,60 @@
 <template>
-  
+  <div class="admin-content">
+    <h1>Проекты</h1>
+    <button class="btn btn-green mb-2">Добавить проекты</button>
+    <p class="c-black mb-1">Показаны записи <strong>1-4</strong> из <strong>4</strong>.</p>
+    <table class="table">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Название</th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in data">
+          <td>{{item.id}}</td>
+          <td>{{item.name}}</td>
+          <td>
+            <div class="btn-group">
+              <button class="btn btn-outline-dark">
+                <i class="fa fa-eye"></i>
+              </button>
+              <button class="btn btn-outline-dark">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-outline-dark">
+                <i class="fa fa-close"></i>
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "AdminProjects"
+    name: "AdminProjects",
+    data() {
+      return {
+        data: [
+          {
+            id: 1,
+            name: 'name1'
+          },
+          {
+            id: 2,
+            name: 'name2'
+          },
+          {
+            id: 3,
+            name: 'name3'
+          }
+        ]
+      }
+    }
   }
 </script>
 
--- src/views/layouts/MainLayout.vue
+++ src/views/layouts/MainLayout.vue
@@ -19,8 +19,51 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss">
   .admin-wrap {
     display: flex;
   }
+  .admin-content {
+    padding: 15px;
+  }
+  .table {
+    margin-bottom: 0;
+    thead {
+      th {
+        border-top: 0;
+        border-bottom-width: 1px;
+        font-weight: 500;
+        font-size: 1rem;
+        i {
+          margin-left: 0.325rem;
+        }
+      }
+    }
+    th,
+    td {
+      padding: 0.9375rem;
+      vertical-align: middle;
+      line-height: 1;
+    }
+    td {
+      font-size: 0.875rem;
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 100%;
+    }
+    .badge {
+      margin-bottom: 0;
+    }
+    }
+    &.table-borderless {
+       border: none;
+      tr,
+      td,
+      th {
+        border: none;
+      }
+    }
+  }
+
 </style>
